3b14be0a0757948ee722e9159754f7702e931689
KFX <KLEITON.FREITAS@LIVE.COM>
2021-09-16 12:02:17 -0300

    insert install chrome
    add install chrome and repo

--- admin_linux.sh
+++ admin_linux.sh
@@ -87,6 +87,10 @@ echo "================================================"
       snap install slack --classic
       wget "https://discordapp.com/api/download?platform=linux&format=deb" -O discord.deb
       dpkg -i discord.deb
+      sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+      wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+      apt-get update
+      apt-get install google-chrome-stable
       sleep 5
 echo "================================================"
 ;;
