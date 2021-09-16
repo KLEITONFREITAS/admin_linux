938a366eeca9a230fa1270e37b8f1cb2571d4baf
KFX <KLEITON.FREITAS@LIVE.COM>
2021-09-16 12:48:28 -0300

    -y
    iyes

--- admin_linux.sh
+++ admin_linux.sh
@@ -90,7 +90,7 @@ echo "================================================"
       sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
       wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
       apt-get update
-      apt-get install google-chrome-stable
+      apt-get install google-chrome-stable -y
       sleep 5
 echo "================================================"
 ;;
